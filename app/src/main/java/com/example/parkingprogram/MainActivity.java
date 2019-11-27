1ce24020a0a1a3d229af48496ac193bc3905cd93
ArturMugin <47824482+ArturMugin@users.noreply.github.com>
2019-11-27 20:10:03 +0000

    Reg is not conected to firebase and can add users + added register window

--- app/src/main/java/com/example/parkingprogram/MainActivity.java
+++ app/src/main/java/com/example/parkingprogram/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.parkingprogram;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -16,6 +18,7 @@ import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.example.parkingprogram.ui.Models.User;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
@@ -78,7 +81,83 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showSignInWindow();
+
+            }
+        });
     }
+
+
+
+
+    private void showSignInWindow(){
+
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Login");
+        dialog.setMessage("Enter details");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View sign_in_Window = inflater.inflate(R.layout.sign_on_window, null);
+        dialog.setView(sign_in_Window);
+
+        final MaterialEditText email = sign_in_Window.findViewById(R.id.emailField);
+        final MaterialEditText password = sign_in_Window.findViewById(R.id.PasswordField);
+
+        dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+
+        dialog.setPositiveButton("Login", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if(TextUtils.isEmpty(email.getText().toString())){
+
+                    Snackbar.make(root, "Enter your email", Snackbar.LENGTH_SHORT).show();
+                    return;
+
+                }
+
+                if(TextUtils.isEmpty(password.getText().toString())){
+
+                    Snackbar.make(root, "Enter your password", Snackbar.LENGTH_SHORT).show();
+                    return;
+
+                }
+
+                auth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                startActivity(new Intent(MainActivity.this, MapActivity.class));
+                                finish();
+                            }
+                        }). addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Snackbar.make(root, "Error logging in." + e.getMessage(), Snackbar.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
+
+        dialog.show();
+
+    }
+
+
+
+
+
 
     private void showRegisterWindow() {
 
@@ -142,7 +221,7 @@ public class MainActivity extends AppCompatActivity {
                                 user.setCarreg(carreg.getText().toString());
                                 user.setUsername(username.getText().toString());
 
-                                users.child(user.getEmail())
+                                users.child(FirebaseAuth.getInstance().getCurrentUser().getUid())
                                         .setValue(user)
                                         .addOnSuccessListener(new OnSuccessListener<Void>() {
                                             @Override
