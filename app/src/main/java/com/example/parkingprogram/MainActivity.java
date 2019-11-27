9b765b7d00305b2b92c417b664bda23f1336eb7e
ArturMugin <47824482+ArturMugin@users.noreply.github.com>
2019-11-27 19:17:19 +0000

    Reg is not conected to firebase and can add users + added register window

--- app/src/main/java/com/example/parkingprogram/MainActivity.java
+++ app/src/main/java/com/example/parkingprogram/MainActivity.java
@@ -1,16 +1,28 @@
 package com.example.parkingprogram;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.example.parkingprogram.ui.Models.User;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
     FirebaseAuth auth;
     FirebaseDatabase db;
     DatabaseReference users;
+
+    ConstraintLayout root;
 
 
 
@@ -52,8 +66,120 @@ public class MainActivity extends AppCompatActivity {
 
         btnSignIn = findViewById(R.id.button11);
         btnRegister = findViewById(R.id.btnRegister);
+        root = findViewById(R.id.root_element);
+        auth =  FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+        users = db.getReference("Users");
 
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showRegisterWindow();
+
+            }
+        });
+    }
+
+    private void showRegisterWindow() {
+
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Register");
+        dialog.setMessage("Enter details");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View registerWindow = inflater.inflate(R.layout.register_window, null);
+        dialog.setView(registerWindow);
+
+        final MaterialEditText email = registerWindow.findViewById(R.id.emailField);
+        final MaterialEditText username = registerWindow.findViewById(R.id.usernameField);
+        final MaterialEditText password = registerWindow.findViewById(R.id.PasswordField);
+        final MaterialEditText carreg = registerWindow.findViewById(R.id.carField);
+
+        dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+
+        dialog.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if(TextUtils.isEmpty(email.getText().toString())){
+
+                    Snackbar.make(root, "Enter your email", Snackbar.LENGTH_SHORT).show();
+                    return;
+
+                }
+                if(TextUtils.isEmpty(username.getText().toString())){
+
+                    Snackbar.make(root, "Enter your username", Snackbar.LENGTH_SHORT).show();
+                    return;
+
+                }
+                if(TextUtils.isEmpty(password.getText().toString())){
+
+                    Snackbar.make(root, "Enter your password", Snackbar.LENGTH_SHORT).show();
+                    return;
+
+                }
+                if(TextUtils.isEmpty(carreg.getText().toString())){
+
+                    Snackbar.make(root, "Enter your car registration number", Snackbar.LENGTH_SHORT).show();
+                    return;
+
+                }
+
+                //register user
+                auth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                User user = new User();
+                                user.setEmail(email.getText().toString());
+                                user.setPassword(password.getText().toString());
+                                user.setCarreg(carreg.getText().toString());
+                                user.setUsername(username.getText().toString());
+
+                                users.child(user.getEmail())
+                                        .setValue(user)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Snackbar.make(root, "User has been added", Snackbar.LENGTH_SHORT).show();
+                                            }
+                                        });
+                            }
+                        });
+            }
+        });
+
+        dialog.show();
 
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
