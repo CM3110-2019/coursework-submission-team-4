79f1a39dd0045a0ae75619a5da6d54fbfbe91cc3
Jamie Chapman <34374063+Jamiealejandro@users.noreply.github.com>
2019-12-05 19:05:08 +0000

    Update SharedPrefActivity.java

--- app/src/main/java/com/example/parkingprogram/SharedPrefActivity.java
+++ app/src/main/java/com/example/parkingprogram/SharedPrefActivity.java
@@ -1,34 +1,41 @@
 package com.example.parkingprogram;
 
+import android.content.SharedPreferences;
+import android.location.Location;
 import android.os.Bundle;
-import android.util.Log;
+
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.CompoundButton;
-import android.widget.Toast;
+
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
 
 public class SharedPrefActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener, AdapterView.OnItemSelectedListener {
 
     private SharedPreferences sharedPrefs;
+    private List<Location>  favourites;
+    private String favourite;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
 
         //initiate sharedPrefs
-        this.sharedPrefs = getSharedPreferences(getString(R.string.shared_prefs_filename), MODE_PRIVATE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main6);
+
+        this.sharedPrefs = getSharedPreferences(getString(R.string.shared_pref_defaultFavourite), MODE_PRIVATE);
 
 
         //set all the favourite car parks with ids that are strings in the shared preferences string set
-        Set<String> selectedFavouritesIds = sharedPrefs.getStringSet(getString(R.string.shared_pref_filename), new HashSet<String>());
-        for (Favourite favourite : favourites) {
-            if (selectedFavouritesIds.equals(favourites.getId())){
+        Set<String> selectedFavouritesIds = sharedPrefs.getStringSet(getString(R.string.shared_pref_defaultFavourite), new HashSet<String>());
+        for (Location favourite : favourites) {
+            if (selectedFavouritesIds.equals(favourites.getId())) {
                 favourite.setSelected(true);
                 break;
             }
@@ -38,19 +45,38 @@ public class SharedPrefActivity extends AppCompatActivity implements CompoundBut
 
     @Override
     protected void onPause() {
+
+
         super.onPause();
         SharedPreferences.Editor sharedPrefsEditor = this.sharedPrefs.edit();
+
+
         Set<String> selectedFavouriteIds = new HashSet <String>();
 
         //store favourite car park that has been selected
-        for (Favourite favourite: this.favourites) {
+        for (Location favourite: this.favourites) {
             if (favourite.isSelected()){
                 selectedFavouriteIds.add(favourite.getId());
             }
         }
-        sharedPrefsEditor.putStringSet(R.string.shared_pref_filename), selectedFavouriteIds);
+        sharedPrefsEditor.putStringSet(getString(R.string.shared_pref_defaultFavourite), selectedFavouriteIds);
 
         //apply changes
         sharedPrefsEditor.apply();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
     }
 }
