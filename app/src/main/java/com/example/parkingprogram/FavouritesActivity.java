6f8157b8168b699f7bea2a07a8cc9ed973b7c19c
Jamie Chapman <34374063+Jamiealejandro@users.noreply.github.com>
2019-12-05 14:07:59 +0000

    Update FavouritesActivity.java
    dont pull, have not tested yet, need it for home so i can test there

--- app/src/main/java/com/example/parkingprogram/FavouritesActivity.java
+++ app/src/main/java/com/example/parkingprogram/FavouritesActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Main6Activity extends AppCompatActivity {
+public class FavouritesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main6);
+
+        recyclerView = findViewById(R.id.);
+        FavouritesRecyclerViewAdapter adapter = new FavouritesRecyclerViewAdapter(getApplicationContext, favourites);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinerLayoutManager(getApplicationContext()));
+
     }
 }
